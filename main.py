fd0024781d4c61c0739559eff4270646c1f3f01b
DizzyFlames <dizzyflames@hotmail.com>
2021-05-19 23:02:29 -0400

    removed unnecessary search box

--- main.py
+++ main.py
@@ -52,11 +52,6 @@ def getMeaning(file, directory):
 
 file_list_column = [
     [
-        sg.Text("Image Folder"),
-        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
-        sg.FolderBrowse(),
-    ],
-    [
         sg.Listbox(
             values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
         )
